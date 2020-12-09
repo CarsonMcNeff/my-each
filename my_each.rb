51cbe360db0d210d1c7b235ce170a9ccae310dd1
CarsonMcNeff <cjmcneff@outlook.com>
2020-12-09 13:34:24 -0600

    commit tree d5aa4b3e2a1ae2564154d598141f512c014c467d

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  newarray = []
+  i = 0
+  while i < array.length
+    yield array[i]
+    i += 1
+    newarray.push(array[i-1])
+  end
+  newarray
 end
